dddf61a4fe34740bd7172adccfd794a1a28d7dfd
Erich Purpur <epurpur@gmail.com>
2021-07-06 10:05:37 -0400

    changing state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react';
 
 
 function App() {
+
+  // handles showing the add task form or not by clicking 'add' button
+  const [showAddTask, setShowAddTask] = useState(true)
 
   // tasks are now a part of app (component) level state
   const [tasks, setTasks] = useState([
@@ -27,6 +31,16 @@ function App() {
     },
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = {id, ...task}
+
+    //add newTask to array of tasks. copy array of current tasks and add newTask to it.
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     // filters out tasks that are not equal to id of the one that is clicked on the UI
@@ -34,15 +48,17 @@ function App() {
   }
 
   // Toggle Reminder
+  // toggles green bar next to task to remind you about it
   const toggleReminder = (id) => {
-    console.log(id)
+    // maps over all tasks.  If the task.id === the one user clicks on, set that particular task.reminder to the opposite of its current value
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
   // can only return 1 element in JSX expression.  Can have others nested inside
   return (
     <div className="container">
-      <Header title='Task Tracker' />
-
+      <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {/* Using ternary operator. If tasks, show them. If not show 'no tasks to show' */}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
